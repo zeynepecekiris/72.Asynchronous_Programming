68531ecceec0b0f1be1a0eff868e2f05c30556e1
zeynepecekiris <zeynep.ece933@gmail.com>
2024-07-30 19:36:53 +0300

    first commit

---
+++ synchronousVSAsynchronous.js
@@ -0,0 +1,19 @@
+// synchronous bilet gisesi
+// asynchronous mc donalds menusu
+
+
+//AsynCode 
+
+console.log(1);
+console.log(2);
+console.log(3);
+
+setTimeout(() =>{
+    console.log("ece");
+}, 20000);
+console.log(4);
+console.log(5);
+//suan sekronize ilerliyor, asyn olarak nasil calisir; 2-3 arasina settimeout
+//kullanabilirsin. Arrow f.yapistirdin,icerisine baska bi console.log yaz.saniyeyi ilk suslu parantez bittikten sonra saniye atanir ve 
+//bu olaydan sonra kodun asyn olarak calismis olucak.
+
